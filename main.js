48833d911dc08fe2e8df1dc86465a8ffdd9404f9
Angie Cabanillas <acabanillasb2@upao.edu.pe>
2022-09-05 23:52:29 -0500

    chore: :memo: clases en javascript

--- main.js
+++ main.js
@@ -37,3 +37,53 @@ const juana = new Student(
     22,
     ['curso1', 'curso2', 'curso3']
 );
+
+//prototipos con la sintazis de clases
+//CLASES
+class Student2{//1°forma de declara clase(atributos ordenados)
+    constructor(name, age, cursosAprobados){
+        this.name = name;
+        this.age = age;
+        this.cursosAprobados = cursosAprobados;
+    }
+
+    aprobarCurso(nuevoCurso){
+        this.cursosAprobados.push(nuevoCurso);
+    }
+}
+
+//creamos una instancia de la clase student2
+const angie = new Student2(
+    'Angie',
+    15,
+    [
+        'curso basico de html y css', 
+        'curso practico de html y css'
+    ]
+);
+
+//
+///2° forma: declarar una clase (atributos desordenados)
+class Student2{
+    constructor({//recibir un solo parametro tipo objeto
+        age, name, email, cursosAprobados = []//iniciamos nuestro arreglo vacio
+    }){
+        this.name = name;
+        this.age = age;
+        this.cursosAprobados = cursosAprobados;
+    }
+
+    aprobarCurso(nuevoCurso){
+        this.cursosAprobados.push(nuevoCurso);
+    }
+}
+
+//creamos una instancia de la clase student2
+const angie2 = new Student2({//los atributos pueden asignarse en desorden
+    name:'Angie',
+    age: 15,
+    cursosAprobados : [
+        'curso basico de html y css', 
+        'curso practico de html y css'
+    ]
+});
