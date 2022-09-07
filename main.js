a5ce55bacd5053b9131adfb2ddcec8daea7edfe5
Angie Cabanillas <acabanillasb2@upao.edu.pe>
2022-09-07 07:40:35 -0500

    chore: :zap: Ventajas de la POO

--- main.js
+++ main.js
@@ -38,7 +38,7 @@ const juana = new Student(
     ['curso1', 'curso2', 'curso3']
 );
 
-//prototipos con la sintazis de clases
+//prototipos con la sintaxis de clases
 //CLASES
 class Student2{//1°forma de declara clase(atributos ordenados)
     constructor(name, age, cursosAprobados){
@@ -85,5 +85,76 @@ const angie2 = new Student2({//los atributos pueden asignarse en desorden
     cursosAprobados : [
         'curso basico de html y css', 
         'curso practico de html y css'
+    ]
+});
+
+//------------------------------------------------------------------
+//VENTAJAS DE LA PROGRAMACION ORIENTADA A OBJETOS
+
+
+//creamos clase de rutas
+class LearningPaths{
+    constructor({
+        nombre, cursos = []
+    }){
+        this.nombre = nombre;
+        this. cursos = cursos;
+    }    
+}
+//instancia de LearningPaths
+const escuelaWeb = new LearningPaths({
+    nombre:'Escuela de desarrollo web',
+    cursos:['curso basico de html y css', 'curso practico de html y css']
+});
+const escuelaDiseño = new LearningPaths({
+    nombre:'Escuela de diseño',
+    cursos:['curso de diseño UI', 'curso practico de UX']
+});
+
+//creamos clases
+class Student{
+    constructor({
+        name,
+        email,
+        username,
+        twitter = undefined,//si no envian nada, se crean como undefined
+        instagram = undefined,
+        facebook = undefined,
+        approvedCourses= [],
+        learningPaths = [],
+    }){
+        this.name = name;
+        this.email = email;
+        this.username = username;
+        this.socialMedia = {
+            twitter,
+            instagram,
+            facebook,
+        }
+        this.approvedCourses = approvedCourses;
+        this.learningPaths = learningPaths;
+    }
+}
+
+//creamos instancia de la clase
+const alessio = new Student({
+    name:'Alessio',
+    email:'angiecb9@hotmail.com',
+    username:'alexandra',
+    twitter: 'holi',
+    learningPaths:[
+        escuelaWeb,
+    ]
+});
+
+//creamos instancia de la clase
+const alessio2 = new Student({
+    name:'Alessio2',
+    email:'angiecb9@hotmail.com',
+    username:'alexandra',
+    facebook: 'facedealessio',
+    learningPaths:[
+        escuelaDiseño, 
+        escuelaWeb
     ]
 });
